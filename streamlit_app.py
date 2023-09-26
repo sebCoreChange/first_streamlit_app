f46b276732e910f98c15307688c279efd2dc4922
sebCoreChange <144214777+sebCoreChange@users.noreply.github.com>
2023-09-26 07:49:37 +0000

    Added selector to fruit info.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,12 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
+# Paramter to select fruit info.
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"  +  "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"  +  fruit_choice )
 
 # Parse the reponse to a panadas data frame. 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
