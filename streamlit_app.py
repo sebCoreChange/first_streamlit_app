92903ca736dd9df209937d90cc799effc889402c
sebCoreChange <144214777+sebCoreChange@users.noreply.github.com>
2023-09-26 07:47:00 +0000

    Kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Lime', 'Apple'] )
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Kiwi', 'Apple'] )
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 
@@ -26,8 +26,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"  +  "kiwi")
 
 # Parse the reponse to a panadas data frame. 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
